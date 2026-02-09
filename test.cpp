5bad331b823de0464570bb34c092a29a14941b31
LAPTOP-OBQO5216\PC-YUUTO <zen-yuuto@amami-joho.ac.jp>
2026-02-09 12:04:11 +0900

    ゲームクリア判定

--- test.cpp
+++ test.cpp
@@ -7,10 +7,11 @@ typedef enum {
 } CellType;
 
 int keyCheck(void);
-void moveCharacter(int key, int* x, int* y, int* a);
+void moveCharacter(int key, int* x, int* y, int* a, int* itemCount);
 void drawBoard();
 void drawCharacter(int x, int y, int a, int handle);
 void initMap(void);
+int getTotalItems(void);
 
 #define WINDOWSIZE 800
 #define CELLSIZE 40
@@ -63,6 +64,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	int x = 1, y = 2; //　キャラクターの位置（座標ではなく、マス目の位置）
 	int a = 0; // 0:右、1:下、2:左、3:上
 
+	int itemCount = getTotalItems(); // マップ内に配置されたアイテムの総数を取得
+
 	int handle = LoadGraph("chara1.png");
 
 	do {
@@ -70,9 +73,18 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		drawBoard();
 		drawCharacter(x, y, a, handle);
 
+		if (itemCount == 0) {	//ゲームクリア判定
+			SetFontSize(64);
+			const char* str = "GAME CLEAR!";
+			int w = GetDrawStringWidth(str, strlen(str));
+			DrawString((WINDOWSIZE - w) / 2, (WINDOWSIZE - 64) / 2, str, GetColor(255, 255, 0));
+			ScreenFlip();
+			break;
+		}
+
 		int key = keyCheck();
 
-		moveCharacter(key, &x, &y, &a);
+		moveCharacter(key, &x, &y, &a, &itemCount);
 		ProcessMessage();        // メッセージ処理
 	} while (!CheckHitKey(KEY_INPUT_ESCAPE));
 
@@ -156,7 +168,7 @@ int keyCheck() {
 	return key;
 }
 
-void moveCharacter(int key, int* x, int* y, int* a)
+void moveCharacter(int key, int* x, int* y, int* a, int* itemCount)
 {
 	int prevX = *x;
 	int prevY = *y;
@@ -192,5 +204,20 @@ void moveCharacter(int key, int* x, int* y, int* a)
 	/*アイテム取得の処理*/
 	if (board[*x][*y] == CELL_ITEM) {
 		board[*x][*y] = CELL_PATH;
+		(*itemCount)--;
 	}
 }
+
+int getTotalItems()
+{
+	int count = 0;
+	for (int y = 0; y < CELLS; y++) {
+		for (int x = 0; x < CELLS; x++) {
+			if (board[x][y] == CELL_ITEM) {
+				count++;
+			}
+		}
+	}
+	return count;
+}
+
