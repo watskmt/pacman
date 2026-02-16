5f4c79131af1751443f825277d2672f171153729
R7600\watsk <watskmt@gmail.com>
2026-02-16 11:55:29 +0900

    変更部分のみのコミット

--- test.cpp
+++ test.cpp
@@ -1,47 +1,63 @@
 ﻿#include "DxLib.h"
 
+/*--------------------
+  定数・型定義
+--------------------*/
 typedef enum {
 	CELL_PATH = 0,
 	CELL_WALL = 1
 } CellType;
 
+/*--------------------
+  関数プロトタイプ
+--------------------*/
 int keyCheck(void);
-void moveCharacter(int key, int* x, int* y, int* a);
 void drawBoard();
-void drawCharacter(int x, int y, int a, int handle);
+void moveCharacter(int key, int* x, int* y, int* a, double* scale);
+void drawCharacter(int x, int y, int a, int handle, double scale);
+void drawItems(int powerHandle, int smallHandle);
 void initMap(void);
 
 #define WINDOWSIZE 800
 #define CELLSIZE 40
-#define CELLS WINDOWSIZE / CELLSIZE
-#define PI    3.1415926535897932384626433832795f
+#define CELLS (WINDOWSIZE / CELLSIZE)
+#define PI 3.1415926535897932384626433832795f
 CellType board[CELLS][CELLS] = { CELL_PATH };
 
 
-const char level[CELLS][CELLS + 1] = {
+/*--------------------
+  グローバル変数
+--------------------*/
+
+char level[CELLS][CELLS + 1] = {
 	"##########..########",
 	"#...#..............#",
-	"#...#..............#",
+	"#...#............1.#",
 	"#...############...#",
 	"#..............#...#",
-	"#..............#...#",
+	"#.....1........#...#",
 	"#..................#",
 	"#...##########.....#",
 	"#..................#",
-	"#.....###..........#",
+	"#.....###......1...#",
 	"#........######....#",
-	"#..................#",
+	"#..2...............#",
 	"#...####....####...#",
 	"#..#.....##.....#..#",
 	"#...#..........#...#",
 	"#....#........#....#",
 	"#......#.....#.....#",
-	"#.........#........#",
+	"#.........#....2...#",
 	"#..................#",
 	"##########..########",
 };
 
-// プログラムは WinMain から始まります
+int itemPowerHandle;
+int itemSmallHandle;
+
+/*--------------------
+  メイン関数
+--------------------*/
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	initMap();
@@ -55,131 +71,174 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	SetMouseDispFlag(TRUE);
 	SetGraphMode(WINDOWSIZE, WINDOWSIZE, 32); // 画面サイズを640x480、色深度32ビットに設定
 
-	WaitKey();				// キー入力待ち
+	int charHandle = LoadGraph("chara1.png");
+	itemPowerHandle = LoadGraph("item_power.png");
+	itemSmallHandle = LoadGraph("item_small.png");
 
-	unsigned int cy = GetColor(200, 200, 100);
+	double scale = 1.0; // 大きさ
 
 	int x = 1, y = 2; //　キャラクターの位置（座標ではなく、マス目の位置）
 	int a = 0; // 0:右、1:下、2:左、3:上
+	while (!CheckHitKey(KEY_INPUT_ESCAPE)) {
+		ClearDrawScreen();
 
-	int handle = LoadGraph("chara1.png");
-
-	do {
-		ClearDrawScreen(); // 画面をクリア
 		drawBoard();
-		drawCharacter(x, y, a, handle);
+		drawItems(itemPowerHandle, itemSmallHandle);
+		drawCharacter(x, y, a, charHandle, scale);
 
 		int key = keyCheck();
+		moveCharacter(key, &x, &y, &a, &scale);
 
-		moveCharacter(key, &x, &y, &a);
-		ProcessMessage();        // メッセージ処理
-	} while (!CheckHitKey(KEY_INPUT_ESCAPE));
+		ProcessMessage();
+	}
 
-		WaitKey();				// キー入力待ち
-
-		DxLib_End();				// ＤＸライブラリ使用の終了処理
-
-	return 0;				// ソフトの終了 
+	DxLib_End();
+	return 0;
 }
 
-void initMap(void) {
+/*--------------------
+  マップ初期化
+--------------------*/
+void initMap(void)
+{
 	for (int y = 0; y < CELLS; y++) {
 		for (int x = 0; x < CELLS; x++) {
-			switch (level[y][x]) {
-			case '#': board[x][y] = CELL_WALL; break;
-			case '.': board[x][y] = CELL_PATH; break;
-			default:  board[x][y] = CELL_PATH; break;
-			}
-		}
-	}
-}
-
-void drawBoard() {
-	unsigned int c;
-	int fillFlag;
-
-	for (int i = 0; i < CELLS; i++)
-		for (int j = 0; j < CELLS; j++) {
-			if (board[j][i] == CELL_WALL) {
-			 c = GetColor(255, 100, 255);
-				fillFlag = TRUE;
+			if (level[y][x] == '#') {
+				board[x][y] = CELL_WALL;
 			}
 			else {
-				c = GetColor(50, 50, 100);
-				fillFlag = FALSE;
+				board[x][y] = CELL_PATH;
 			}
-			DrawBox(j * CELLSIZE, i * CELLSIZE, j * CELLSIZE + CELLSIZE, i * CELLSIZE + CELLSIZE, c, fillFlag);    // 四角形を描画
 		}
-
+	}
 }
 
-void drawCharacter(int x, int y, int a, int handle) {
-	double rad = a == 2 ? 0 : (double)a * PI / 2.0;
-	int turn = a == 2 ? TRUE : FALSE; // 左向きのときだけ反転
-	DrawRotaGraph3(x * CELLSIZE + CELLSIZE / 2, y * CELLSIZE + CELLSIZE / 2, 150, 150, (double)CELLSIZE / 300, (double)CELLSIZE / 300, rad, handle, TRUE, turn);
+/*--------------------
+  マップ描画
+--------------------*/
+void drawBoard(void)
+{
+	for (int y = 0; y < CELLS; y++) {
+		for (int x = 0; x < CELLS; x++) {
+
+			int color;
+			int fill;
+
+			if (board[x][y] == CELL_WALL) {
+				color = GetColor(255, 100, 255);
+				fill = TRUE;
+			}
+			else {
+				color = GetColor(50, 50, 100);
+				fill = FALSE;
+			}
+
+			DrawBox(
+				x * CELLSIZE,
+				y * CELLSIZE,
+				x * CELLSIZE + CELLSIZE,
+				y * CELLSIZE + CELLSIZE,
+				color,
+				fill
+			);
+		}
+	}
 }
 
-// 戻り値：　０：押されていない、1：上、2：下、3：左、4：右
-int keyCheck() {
-	static int upKey = 0, downKey = 0, leftKey = 0, rightKey = 0;
+/*--------------------
+  アイテム描画
+--------------------*/
+void drawItems(int powerHandle, int smallHandle)
+{
+	for (int y = 0; y < CELLS; y++) {
+		for (int x = 0; x < CELLS; x++) {
 
+			int px = x * CELLSIZE + CELLSIZE / 2;
+			int py = y * CELLSIZE + CELLSIZE / 2;
+
+			if (level[y][x] == '1') {
+				DrawRotaGraph(px, py, 0.5, 0.0, powerHandle, TRUE);
+			}
+			else if (level[y][x] == '2') {
+				DrawRotaGraph(px, py, 0.5, 0.0, smallHandle, TRUE);
+			}
+		}
+	}
+}
+
+/*--------------------
+  キャラ描画
+--------------------*/
+void drawCharacter(int x, int y, int a, int handle, double scale)
+{
+	double rad = (a == 2) ? 0.0 : (double)a * PI / 2.0;
+	int turn = (a == 2);
+
+	DrawRotaGraph3(
+		x * CELLSIZE + CELLSIZE / 2,
+		y * CELLSIZE + CELLSIZE / 2,
+		150, 150,
+		(double)CELLSIZE / 300 * scale,
+		(double)CELLSIZE / 300 * scale,
+		rad,
+		handle,
+		TRUE,
+		turn
+	);
+}
+
+/*--------------------
+  キー入力
+--------------------*/
+int keyCheck(void)
+{
+	static int u, d, l, r;
 	int key = 0;
-	int upKey0 = CheckHitKey(KEY_INPUT_UP);
-	int downKey0 = CheckHitKey(KEY_INPUT_DOWN);
-	int leftKey0 = CheckHitKey(KEY_INPUT_LEFT);
-	int rightKey0 = CheckHitKey(KEY_INPUT_RIGHT);
 
-	if (!upKey && upKey0) {
-		key = 1;
-	}
-	if (!downKey && downKey0) {
-		key = 2;
-	}
-	if (!leftKey && leftKey0) {
-		key = 3;
-	}
-	if (!rightKey && rightKey0) {
-		key = 4;
-	}
+	if (!u && CheckHitKey(KEY_INPUT_UP)) key = 1;
+	if (!d && CheckHitKey(KEY_INPUT_DOWN)) key = 2;
+	if (!l && CheckHitKey(KEY_INPUT_LEFT)) key = 3;
+	if (!r && CheckHitKey(KEY_INPUT_RIGHT)) key = 4;
 
-	upKey = upKey0;
-	downKey = downKey0;
-	leftKey = leftKey0;
-	rightKey = rightKey0;
+	u = CheckHitKey(KEY_INPUT_UP);
+	d = CheckHitKey(KEY_INPUT_DOWN);
+	l = CheckHitKey(KEY_INPUT_LEFT);
+	r = CheckHitKey(KEY_INPUT_RIGHT);
 
 	return key;
 }
 
-void moveCharacter(int key, int* x, int* y, int* a)
+/*--------------------
+  キャラ移動＆アイテム取得
+--------------------*/
+void moveCharacter(int key, int* x, int* y, int* a, double* scale)
 {
-	int prevX = *x;
-	int prevY = *y;
+	int px = *x;
+	int py = *y;
+
 	switch (key) {
-	case 1: // UP
-		(*y)--;
-		*a = 3;
-		break;
-	case 2: // DOWN
-		(*y)++;
-		*a = 1;
-		break;
-	case 3: // LEFT
-		(*x)--;
-		*a = 2;
-		break;
-	case 4: // RIGHT
-		(*x)++;
-		*a = 0;
-		break;
-	default:
-		break;
+	case 1: (*y)--; *a = 3; break;
+	case 2: (*y)++; *a = 1; break;
+	case 3: (*x)--; *a = 2; break;
+	case 4: (*x)++; *a = 0; break;
+	default: return;
 	}
-	if (*x < 0 || *x >= CELLS || *y < 0 || *y >= CELLS) {
-		*x = prevX;
-		*y = prevY;
+
+	if (*x < 0 || *x >= CELLS || *y < 0 || *y >= CELLS ||
+		board[*x][*y] == CELL_WALL) {
+		*x = px;
+		*y = py;
+		return;
 	}
-	if (board[*x][*y] == CELL_WALL) {
-		*x = prevX;
-		*y = prevY;
+
+	if (level[*y][*x] == '1') {
+		*scale += 0.2;
+		if (*scale > 2.0) *scale = 2.0;
+		((char*)level)[*y * (CELLS + 1) + *x] = '.';
+	}
+	else if (level[*y][*x] == '2') {
+		*scale -= 0.2;
+		if (*scale < 0.4) *scale = 0.4;
+		((char*)level)[*y * (CELLS + 1) + *x] = '.';
 	}
 }
